23c1170e3914a270746148f30509e2bcd25c6d80
mglassner <maxi.glassner@gmx.de>
2017-05-23 14:52:55 +0200

    Aufgabe8
    FirstSteps

--- 08_FirstSteps/Core/FirstSteps.cs
+++ 08_FirstSteps/Core/FirstSteps.cs
@@ -16,11 +16,122 @@ namespace Fusee.Tutorial.Core
 {
     public class FirstSteps : RenderCanvas
     {
+        private TransformComponent _cubeTransform1;
+        private TransformComponent _cubeTransform2;
+        private TransformComponent _cubeTransform3;
+        private TransformComponent _cubeTransform4;
+        private TransformComponent _cubeTransform5;
+        private SceneContainer _scene;
+        private SceneRenderer _sceneRenderer;
+        private float _camAngle = 0;
+
         // Init is called on startup. 
         public override void Init()
         {
             // Set the clear color for the backbuffer to white (100% intentsity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(1, 1, 1, 1);
+            RC.ClearColor = new float4(0f, 0.139f, 0.139f, 1);
+
+            // Create a scene with a cube
+            // The three components: one XForm, one Material and the Mesh
+            _cubeTransform1 = new TransformComponent { Scale = new float3(1, 1, 1), Translation = new float3(0, 0, 0) };
+            var cubeMaterial1 = new MaterialComponent
+            {
+
+                Diffuse = new MatChannelContainer { Color = new float3(0.2f, 0.2f, 0.2f) },
+                Specular = new SpecularChannelContainer { Color = float3.One, Shininess = 4 }
+
+            };
+            var cubeMesh1 = SimpleMeshes.CreateCuboid(new float3(5, 5, 5));
+
+            _cubeTransform2 = new TransformComponent { Scale = new float3(1, 1, 1), Translation = new float3(0, 20, 0) };
+            var cubeMaterial2 = new MaterialComponent
+            {
+
+                Diffuse = new MatChannelContainer { Color = new float3(1f, 0.2f, 0.2f) },
+                Specular = new SpecularChannelContainer { Color = float3.One, Shininess = 4 }
+
+            };
+            var cubeMesh2 = SimpleMeshes.CreateCuboid(new float3(5, 20, 2));
+
+
+            _cubeTransform3 = new TransformComponent { Scale = new float3(1, 1, 1), Translation = new float3(0, -20, 0) };
+            var cubeMaterial3 = new MaterialComponent
+            {
+
+                Diffuse = new MatChannelContainer { Color = new float3(0.2f, 0.2f, 1f) },
+                Specular = new SpecularChannelContainer { Color = float3.One, Shininess = 4 }
+
+            };
+            var cubeMesh3 = SimpleMeshes.CreateCuboid(new float3(5, 20, 2));
+
+            _cubeTransform4 = new TransformComponent { Scale = new float3(1, 1, 1), Translation = new float3(-20, 0, 0) };
+            var cubeMaterial4 = new MaterialComponent
+            {
+
+                Diffuse = new MatChannelContainer { Color = new float3(1f, 1f, 1f) },
+                Specular = new SpecularChannelContainer { Color = float3.One, Shininess = 4 }
+
+            };
+            var cubeMesh4 = SimpleMeshes.CreateCuboid(new float3(10, 5, 5));
+
+            _cubeTransform5 = new TransformComponent { Scale = new float3(1, 1, 1), Translation = new float3(20, 0, 0) };
+            var cubeMaterial5 = new MaterialComponent
+            {
+
+                Diffuse = new MatChannelContainer { Color = new float3(0f, 0f, 0f) },
+                Specular = new SpecularChannelContainer { Color = float3.One, Shininess = 4 }
+
+            };
+            var cubeMesh5 = SimpleMeshes.CreateCuboid(new float3(10, 5, 5));
+
+
+
+
+
+            // Assemble the cube node containing the three components
+            var cubeNode1 = new SceneNodeContainer();
+            cubeNode1.Components = new List<SceneComponentContainer>();
+            cubeNode1.Components.Add(_cubeTransform1);
+            cubeNode1.Components.Add(cubeMaterial1);
+            cubeNode1.Components.Add(cubeMesh1);
+
+            var cubeNode2 = new SceneNodeContainer();
+            cubeNode2.Components = new List<SceneComponentContainer>();
+            cubeNode2.Components.Add(_cubeTransform2);
+            cubeNode2.Components.Add(cubeMaterial2);
+            cubeNode2.Components.Add(cubeMesh2);
+
+            var cubeNode3 = new SceneNodeContainer();
+            cubeNode3.Components = new List<SceneComponentContainer>();
+            cubeNode3.Components.Add(_cubeTransform3);
+            cubeNode3.Components.Add(cubeMaterial3);
+            cubeNode3.Components.Add(cubeMesh3);
+
+            var cubeNode4 = new SceneNodeContainer();
+            cubeNode4.Components = new List<SceneComponentContainer>();
+            cubeNode4.Components.Add(_cubeTransform4);
+            cubeNode4.Components.Add(cubeMaterial4);
+            cubeNode4.Components.Add(cubeMesh4);
+
+            var cubeNode5 = new SceneNodeContainer();
+            cubeNode5.Components = new List<SceneComponentContainer>();
+            cubeNode5.Components.Add(_cubeTransform5);
+            cubeNode5.Components.Add(cubeMaterial5);
+            cubeNode5.Components.Add(cubeMesh5);
+
+            // Create the scene containing the cubes
+            _scene = new SceneContainer();
+            _scene.Children = new List<SceneNodeContainer>();
+            _scene.Children.Add(cubeNode1);
+            _scene.Children.Add(cubeNode2);
+            _scene.Children.Add(cubeNode3);
+            _scene.Children.Add(cubeNode4);
+            _scene.Children.Add(cubeNode5);
+
+            // Create a scene renderer holding the scene above
+            _sceneRenderer = new SceneRenderer(_scene);
+
+
         }
 
         // RenderAFrame is called once a frame
@@ -29,10 +140,29 @@ namespace Fusee.Tutorial.Core
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
+            _camAngle = _camAngle + 0.05f;
+
+            _cubeTransform1.Scale = new float3(1 * M.Sin(4 * TimeSinceStart) + 1.5f, 1 * M.Sin(4 * TimeSinceStart) + 1.5f, 1 * M.Sin(4 * TimeSinceStart) + 1.5f);
+            _cubeTransform1.Translation = new float3(0, 40 * M.Sin((2 * TimeSinceStart) + M.Pi / 4), 0);
+            _cubeTransform2.Translation = new float3(0, 20 + 10 * M.Sin(1 * TimeSinceStart), 0);
+            _cubeTransform3.Translation = new float3(0, -20 - 10 * M.Sin(1 * TimeSinceStart), 0);
+            _cubeTransform4.Translation = new float3(20, 20 * M.Sin(6 * TimeSinceStart), 0);
+            _cubeTransform5.Translation = new float3(-20, 20 * M.Sin(6 * TimeSinceStart), 0);
+
+
+            // Setup the camera 
+            RC.View = float4x4.CreateTranslation(0, 0, 100) * float4x4.CreateRotationY(_camAngle);
+
+
+
+            // Render the scene on the current render context
+            _sceneRenderer.Render(RC);
+
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rerndered farame) on the front buffer.
             Present();
         }
+
 
 
         // Is called when the window was resized
