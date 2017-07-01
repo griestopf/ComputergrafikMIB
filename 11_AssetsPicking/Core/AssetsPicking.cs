a62bdb019fa56ebc1c6cda9944ccc7e7d2bab380
mglassner <maxi.glassner@gmx.de>
2017-07-01 19:43:55 +0200

    Aufgabe 12
    Fahrzeug bewegen

--- 11_AssetsPicking/Core/AssetsPicking.cs
+++ 11_AssetsPicking/Core/AssetsPicking.cs
@@ -16,11 +16,12 @@ namespace Fusee.Tutorial.Core
 {
     public class AssetsPicking : RenderCanvas
     {
-        private SceneContainer _scene;
-        private SceneRenderer _sceneRenderer;        
-        private float _camAngle = 0;
+        private SceneContainer _scene;       
+        private SceneRenderer _sceneRenderer;
+        private float _camAngle = -2.335f;
+        private float _camAngleVelocity = 0;
+        private PickResult _currentPick;
         private TransformComponent _baseTransform;
-
         private TransformComponent _radhintenreTransform;
         private TransformComponent _radhintenliTransform;
         private TransformComponent _radvornereTransform;
@@ -30,47 +31,13 @@ namespace Fusee.Tutorial.Core
         private TransformComponent _AchsevorneTransform;
         private TransformComponent _TowerTransform;
         private TransformComponent _laufTransform;
-        private ScenePicker _scenePicker;
-        private PickResult _currentPick;
+        private TransformComponent _trailerTransform;
+        private ScenePicker _scenePicker;       
         private float3 _oldColor;
+        private float _d = 15;
+        
 
-        SceneContainer CreateScene()
-        {
-            // Initialize transform components that need to be changed inside "RenderAFrame"
-            _baseTransform = new TransformComponent
-            {
-                Rotation = new float3(0, 0, 0),
-                Scale = new float3(1, 1, 1),
-                Translation = new float3(0, 0, 0)
-            };
 
-            // Setup the scene graph
-            return new SceneContainer
-            {
-                Children = new List<SceneNodeContainer>
-                {
-                    new SceneNodeContainer
-                    {
-                        Components = new List<SceneComponentContainer>
-                        {
-                            // TRANSFROM COMPONENT
-                            _baseTransform,
-
-                            // MATERIAL COMPONENT
-                            new MaterialComponent
-                            {
-                                Diffuse = new MatChannelContainer { Color = new float3(0.7f, 0.7f, 0.7f) },
-                                Specular = new SpecularChannelContainer { Color = new float3(1, 1, 1), Shininess = 5 }
-                            },
-
-                            // MESH COMPONENT
-                            // SimpleAssetsPickinges.CreateCuboid(new float3(10, 10, 10))
-                            SimpleMeshes.CreateCuboid(new float3(10, 10, 10))
-                        }
-                    },
-                }
-            };
-        }
 
         // Init is called on startup. 
         public override void Init()
@@ -90,6 +57,17 @@ namespace Fusee.Tutorial.Core
             _TowerTransform = _scene.Children.FindNodes(node => node.Name == "Tower")?.FirstOrDefault()?.GetTransform();
             _laufTransform = _scene.Children.FindNodes(node => node.Name == "lauf")?.FirstOrDefault()?.GetTransform();
 
+            _trailerTransform = new TransformComponent { Rotation = new float3(-M.Pi / 5.7f, 0, 0), Scale = float3.One, Translation = new float3 (0,0,-30)};
+            _scene.Children.Add(new SceneNodeContainer
+            {
+                Components = new List<SceneComponentContainer>
+                {
+                    _trailerTransform,
+                    new MaterialComponent { Diffuse = new MatChannelContainer { Color = new float3(0.7f, 0.7f, 0.7f) }, Specular = new SpecularChannelContainer { Color = new float3(1, 1, 1), Shininess = 5 }},
+                    SimpleMeshes.CreateCuboid(new float3(2, 2, 2))
+                }
+            });
+          
             _scenePicker = new ScenePicker(_scene);
 
             // Create a scene renderer holding the scene above
@@ -104,33 +82,52 @@ namespace Fusee.Tutorial.Core
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
-            //Kamera Bewegen mit der Maus
+            if (_camAngleVelocity > 0)
+            {
+                _camAngleVelocity -= 0.03f;
+                if (_camAngleVelocity < 0)
+                {
+                    _camAngleVelocity = 0;
+                }
+            };
+            if (_camAngleVelocity < 0)
+            {
+                _camAngleVelocity += 0.03f;
+                if (_camAngleVelocity > 0)
+                {
+                    _camAngleVelocity = 0;
+                }
+            };
+
             var maus = Mouse.LeftButton;
             if (maus == true)
             {
                 _camAngle += Mouse.Velocity.x * 0.0001f * DeltaTime / 16 * 1000;
             }
+            _camAngle -= _camAngleVelocity;
 
             //"zoom"-funktion 
-            float achseZ = 50;
-            achseZ += Mouse.Wheel;
+            //float achseZ = 50;
+            //achseZ += Mouse.Wheel;
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, -10, achseZ) * float4x4.CreateRotationY(_camAngle);
+            //RC.View = float4x4.CreateTranslation(0, -10, achseZ) * float4x4.CreateRotationY(_camAngle);
             //RC.View = float4x4.CreateTranslation(0, -10, achseZ) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
-
+            RC.View = float4x4.CreateRotationX(-M.Pi / 7.3f) * float4x4.CreateRotationY(M.Pi - _trailerTransform.Rotation.y) * float4x4.CreateTranslation(-_trailerTransform.Translation.x, -10, -_trailerTransform.Translation.z);
             if (Mouse.RightButton)
             {
                 float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
                 _scenePicker.View = RC.View;
                 _scenePicker.Projection = RC.Projection;
                 List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
-                PickResult newPick = null;
                 if (pickResults.Count > 0)
                 {
-                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
-                    newPick = pickResults[0];
-                }
+                    PickResult newPick = null;
+                    if (pickResults.Count > 0)
+                    {
+                        pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                        newPick = pickResults[0];
+                    }
                     if (newPick?.Node != _currentPick?.Node)
                     {
                         if (_currentPick != null)
@@ -144,84 +141,81 @@ namespace Fusee.Tutorial.Core
                             mat.Diffuse.Color = new float3(2.55f, 0.69f, 0);
                         }
                         _currentPick = newPick;
-                    }               
+                    }
+                }
+
+                else
+                {
+
+                    if (_currentPick != null)
+                    {
+                        _currentPick.Node.GetMaterial().Diffuse.Color = _oldColor;
+                        _currentPick = null;
+
+
+                    }
+                }
+                
             }
 
-            if (_currentPick != null)
+            float lauf = _laufTransform.Rotation.x;
+            if (Keyboard.GetKey(KeyCodes.Down))
             {
-                if (_currentPick.Node.Name == "radhintenli")
+                if (lauf <= 0.2f)
                 {
-                    float radhintenli = _radhintenliTransform.Rotation.x;
-                    radhintenli -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _radhintenliTransform.Rotation = new float3(radhintenli, 0, 0);
+                    lauf += 0.2f * (DeltaTime);
+                    _laufTransform.Rotation = new float3(lauf, 0, 0);
                 }
-
-                if (_currentPick.Node.Name == "radhintenre")
-                {
-                    float radhintenre = _radhintenreTransform.Rotation.x;
-                    radhintenre -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _radhintenreTransform.Rotation = new float3(radhintenre, 0, 0);
-                }
-
-                if (_currentPick.Node.Name == "radvorneli")
-                {
-                    float radvorneli = _radvorneliTransform.Rotation.x;
-                    radvorneli -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _radvorneliTransform.Rotation = new float3(radvorneli, 0, 0);
-                }
-
-                if (_currentPick.Node.Name == "radvornere")
-                {
-                    float radvornere = _radvornereTransform.Rotation.x;
-                    radvornere -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _radvornereTransform.Rotation = new float3(radvornere, 0, 0);
-                }
-
-                if (_currentPick.Node.Name == "Achsehinten")
-                {
-                    float achsehinten = _AchsehintenTransform.Rotation.x;
-                    achsehinten -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _AchsehintenTransform.Rotation = new float3(achsehinten, 0, 0);
-                }
-
-                if (_currentPick.Node.Name == "Achsevorne")
-                {
-                    float achsevorne = _AchsevorneTransform.Rotation.x;
-                    achsevorne -= Keyboard.WSAxis * 2.0f * (DeltaTime);
-                    _AchsevorneTransform.Rotation = new float3(achsevorne, 0, 0);
-                }
-
-                if (_currentPick.Node.Name == "Tower")
-                {
-                    float tower = _TowerTransform.Rotation.y;
-                    tower -= Keyboard.LeftRightAxis * 2.0f * (DeltaTime);
-                    _TowerTransform.Rotation = new float3(0, tower, 0);
-                }
-
-                if (_currentPick.Node.Name == "lauf")
-                {
-                    float lauf = _laufTransform.Rotation.x;
-                    if (Keyboard.GetKey(KeyCodes.Up))
-                    {
-                        if (lauf <= 0.2f)
-                        {
-                            lauf += 0.2f * (DeltaTime);
-                            _laufTransform.Rotation = new float3(lauf, 0, 0);
-                        }
-                    }
-                    if (Keyboard.GetKey(KeyCodes.Down))
-                    {
-                        if (lauf >= -0.2f)
-                        {
-                            lauf -= 0.2f * (DeltaTime);
-                            _laufTransform.Rotation = new float3(lauf, 0, 0);
-                        }
-                    }
-                                        
-                }
-
-
             }
+            if (Keyboard.GetKey(KeyCodes.Up))
+            {
+                if (lauf >= -0.2f)
+                {
+                    lauf -= 0.2f * (DeltaTime);
+                    _laufTransform.Rotation = new float3(lauf, 0, 0);
+                }
+            }
+
+            float tower = _TowerTransform.Rotation.y;
+            tower += Keyboard.LeftRightAxis * 2.0f * (DeltaTime);
+            _TowerTransform.Rotation = new float3(0, tower, 0);
+
+            float xRadhlinks = _radhintenliTransform.Rotation.x;
+            xRadhlinks += Keyboard.WSAxis * 0.09f * (DeltaTime / 16 * 1000);
+            _radhintenliTransform.Rotation = new float3(xRadhlinks, 0, 0);
+
+            float xRadhrechts = _radhintenreTransform.Rotation.x;
+            xRadhrechts += Keyboard.WSAxis * 0.09f * (DeltaTime / 16 * 1000);
+            _radhintenreTransform.Rotation = new float3(xRadhrechts, 0, 0);
+
+            float xRadvlinks = _radvorneliTransform.Rotation.x;
+            float yRadvlinks = _radvorneliTransform.Rotation.y;
+            xRadvlinks += Keyboard.WSAxis * 0.09f * (DeltaTime / 16 * 1000);
+            yRadvlinks = Keyboard.ADAxis * 0.35f;
+            _radvorneliTransform.Rotation = new float3(xRadvlinks, yRadvlinks, 0);
+
+            float xRadvrechts = _radvornereTransform.Rotation.x;
+            float yRadvrechts = _radvornereTransform.Rotation.y;
+            xRadvrechts += Keyboard.WSAxis * 0.09f * (DeltaTime / 16 * 1000);
+            yRadvrechts = Keyboard.ADAxis * .35f;
+            _radvornereTransform.Rotation = new float3(xRadvlinks, yRadvrechts, 0);
+
+            float3 pAalt = _rumpfTransform.Translation;
+            float3 pBalt = _trailerTransform.Translation;
+
+            float posVel = Keyboard.WSAxis * DeltaTime;
+            float rotVel = Keyboard.ADAxis * DeltaTime;
+
+            float newRot = _rumpfTransform.Rotation.y + (rotVel * Keyboard.WSAxis * DeltaTime * 30);
+            _rumpfTransform.Rotation = new float3(0, newRot, 0);
+
+            float3 pAneu = _rumpfTransform.Translation + new float3(posVel * M.Sin(newRot) * 10, 0, posVel * M.Cos(newRot) * 10);
+            _rumpfTransform.Translation = pAneu;
+
+            float3 pBneu = pAneu + (float3.Normalize(pBalt - pAneu) * _d);
+            _trailerTransform.Translation = pBneu;
+
+            _trailerTransform.Rotation = new float3(0, (float)System.Math.Atan2(float3.Normalize(pBalt - pAneu).x, float3.Normalize(pBalt - pAneu).z), 0);
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
