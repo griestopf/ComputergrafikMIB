0b95a09b25eb7efb02aea9368e9d7c75a7cb2bd1
Christoph <Mller>
2019-11-17 16:49:12 +0100

    Tut08_FirstSteps upgraded to reference NuGet Fusee

---
+++ Tut08_FirstSteps/Tut08_FirstSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+using static Fusee.Engine.Core.Time;
+using Fusee.Engine.GUI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuseeApp
+{
+    public class FirstSteps : RenderCanvas
+    {        // Init is called on startup. 
+        public override void Init()
+        {
+            // Set the clear color for the backbuffer to light green (intensities in R, G, B, A).
+            RC.ClearColor = new float4(0.7f, 1.0f, 0.5f, 1.0f);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            Present();
+        }
+    }
+}
